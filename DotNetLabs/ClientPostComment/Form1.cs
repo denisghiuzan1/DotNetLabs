d3e9042f1a787c1dbb92498153a41ab47e319815
Denis Ghiuzan <denis.ghiuzan@endava.com>
2020-03-25 19:05:38 +0200

    Removed dependecines from client

--- DotNetLabs/ClientPostComment/Form1.cs
+++ DotNetLabs/ClientPostComment/Form1.cs
@@ -1,13 +1,8 @@
-﻿using System;
+﻿using PostComment;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using PostComment;
 
 namespace ClientPostComment
 {
